01e4f7e170772f295c7165da28d751d9b64e1263
pcsahu989 <pcsahu989@gmail.com>
2021-08-20 17:59:42 +0530

    Final

--- src/main/java/com/develop2learn/currency/conversion/controller/CurrencyConversionController.java
+++ src/main/java/com/develop2learn/currency/conversion/controller/CurrencyConversionController.java
@@ -2,6 +2,8 @@ package com.develop2learn.currency.conversion.controller;
 
 import com.develop2learn.currency.conversion.bean.CurrencyConversion;
 import com.develop2learn.currency.conversion.proxy.CurrencyExchangeProxy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,9 +20,12 @@ public class CurrencyConversionController {
     @Autowired
     private CurrencyExchangeProxy currencyExchangeProxy;
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyConversionController.class.getName());
+
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}" )
     public CurrencyConversion calculateConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
 
+        logger.info("calculateConversion(...) Calculating conversion value from {} to {}", from, to);
         HashMap<String, String> uriVariables = new HashMap<>();
         uriVariables.put("from", from);
         uriVariables.put("to", to);
@@ -37,6 +42,7 @@ public class CurrencyConversionController {
     @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}" )
     public CurrencyConversion calculateConversionFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
 
+        logger.info("calculateConversionFeign(...) Calculating conversion value from {} to {}", from, to);
         CurrencyConversion currencyConversion = currencyExchangeProxy.getExchangeValue(from, to);
         currencyConversion.setQuantity(quantity);
         currencyConversion.setTotalCalculatedAmount(quantity.multiply(currencyConversion.getConversionMultiple()));
--- src/main/java/com/develop2learn/currency/conversion/proxy/CurrencyExchangeProxy.java
+++ src/main/java/com/develop2learn/currency/conversion/proxy/CurrencyExchangeProxy.java
@@ -6,7 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name = "currency-exchange", url = "localhost:8000")
-@FeignClient(name = "currency-exchange")
+//Kubernetes Change
+//@FeignClient(name = "currency-exchange")
+//@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000") //commenting this because if currency-exchange service is not up while creating currency-conversion, this predefined variable will not available
+@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_URI:http://localhost}:8000") //So using custom environment variables
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
